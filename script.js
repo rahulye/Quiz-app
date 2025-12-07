d404a6437af498ab18f831edfee090564234a0ca
Rahul-krishh <rahulmykris@gmail.com>
2025-12-07 23:09:48 +0530

    feat : implement asyn function logics and interactive

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hey')
+
+console.log('hey');
+
+async function loadQuestions() {
+  const request = await fetch('./questions.json');
+  const questions = await request.json();
+  displayQuestions(questions);
+}
+loadQuestions();
+
+const questionElement = document.querySelector('.js-question');
+const optionsElement = document.querySelector('.js-options');
+
+function displayQuestions(questions) {
+  questionElement.innerHTML = questions.question;
+  questions.options.forEach( (q) => {
+    optionsElement.innerHTML = q.text;
+  });
+};
