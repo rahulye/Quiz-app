3cb127cd99370b3cafe649aca8e8336ef8a086fc
Rahul-krishh <rahulmykris@gmail.com>
2025-12-09 15:03:58 +0530

    feat : add timer and animations

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let questions = [];
 let currentIndex = 0;
 let score = 0;
+let timerID = null;
 
 async function loadQuestions() {
   const request = await fetch('./questions.json');
@@ -14,27 +15,23 @@ const optionsElement = document.querySelector('.js-options');
 const nextElement = document.querySelector('.js-next-btn');
 
 function displayQuestions(currentIndex) {
-
+  startTimer(7000);
   const currentQuestion = questions[currentIndex];
   nextElement.style.display = "none";
   nextElement.onclick = null; 
-  
   questionElement.textContent = currentQuestion.question;
   optionsElement.textContent = "";
-
+  
   currentQuestion.options.forEach( (option) => {
     const newButton = document.createElement("button");
     newButton.innerHTML = option.text;
-
+    
     newButton.addEventListener( 'click' , () => {
+      stopTimerBar();
+      clearTimeout(timerID);
       nextElement.style.display = "flex"; // visible the next btn
       nextElement.onclick = showNext;
-
-      const allButtons = optionsElement.querySelectorAll("button");
-      allButtons.forEach( (btn) => {
-        btn.disabled = true;
-      });
-
+      const allButtons = diableBtn();
       if( option.result === true ) {
         newButton.style.backgroundColor = "green";
         newButton.style.color = "white";
@@ -73,6 +70,34 @@ function showNext() {
     };
   };
 };
+
+function diableBtn() {
+  const allButtons = optionsElement.querySelectorAll("button");
+  allButtons.forEach( (btn) => {
+    btn.disabled = true;
+  });
+  return allButtons;
+};
+
+const bar = document.querySelector('.js-timer-bar');
+function startTimer(duration) {
+  bar.style.transition = "none";
+  bar.style.transform = "scaleX(1)";
+  bar.offsetWidth;
+
+  bar.style.transition = `transform ${duration}ms linear`;
+  bar.style.transform = "scaleX(0)";
+
+  clearTimeout(timerID);
+  timerID = setTimeout(() => {
+    showNext();
+  }, duration);
+
+};
+
+function stopTimerBar() {
+  bar.style.transition = "none";
+}
 
 function shuffleQuestions( array ) {
   return array.sort( ()=> Math.random() - 0.5);
